c4e249f438a644aa14df106aac0506f4cb9e1407
JacobAlb13 <jacopo-albertini@libero.it>
2024-08-06 10:52:32 +0200

    Sito Hamburger

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var $submenu = $("<div id='sub-menu'></div>");
+var $button = $("<div class='button-link'><a class='icon'><i class='fa fa-bars'></i></a></div>");
+$("nav").append($button);
+$("nav").append($submenu);
+$("nav li a").each(function(){
+    var $link = $("<a></a>");
+    $submenu.append($link);
+    $link.attr("href",$(this).attr("href"));
+    $link.text($(this).text());
+});
+$button.click(function(){
+    $($submenu).toggle();
+    
+});
