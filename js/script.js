272240c0a782a7509cbbe737dbd8d015b47e4b7c
JacobAlb13 <jacopo-albertini@libero.it>
2024-08-25 12:10:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-var $submenu = $("<div id='sub-menu'></div>");
-var $button = $("<div class='button-link'><a class='icon'><i class='fa fa-bars'></i></a></div>");
-$("nav").append($button);
-$("nav").append($submenu);
-$("nav li a").each(function(){
-    var $link = $("<a></a>");
-    $submenu.append($link);
-    $link.attr("href",$(this).attr("href"));
-    $link.text($(this).text());
-});
+var $button = $(".button-link");
 $button.click(function(){
-    $submenu.toggle();
-    });
-$(window).resize(function(){
-    var windowWidth = $(window).width();
-    if (windowWidth >= 1020) {  // Dimensioni desktop
-        $submenu.show();  // Mostra il menu
-    } else {   
-            $submenu.hide();  // Nascondi il menu solo se non è aperto
-        }
+    $("nav").toggleClass("sub-menu");
+    
 });
